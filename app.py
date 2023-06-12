eb51fc6b00c559d7d2e4188e4e83436e5fcfd674
e.baruti <63745689+xoniks@users.noreply.github.com>
2023-06-12 18:13:49 +0200

    app added
    a function for addition is added

---
+++ app.py
@@ -0,0 +1,11 @@
+num_1 = float(input("Please enter a number "))
+num_2 = float(input("Please enter a number "))
+
+
+def add_two(num_one, num_two):
+    result = num_one + num_two
+    return result
+
+
+total = add_two(num_1, num_2)
+print("The result is: ", total)
